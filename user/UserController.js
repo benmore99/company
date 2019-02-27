6c03d848266d74387eca4529d221f5a829f33229
BURIN TECHAMA <benmore1992@gmail.com>
2019-02-27 11:55:57 +0700

    upload profile image

--- user/UserController.js
+++ user/UserController.js
@@ -1,11 +1,28 @@
 // UserController.js
+
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
 var bodyParser = require('body-parser');
+
+var User = require('./User');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    cb(null, Date.now() + "." + extension)
+  }
+})
+
+var upload = multer({ storage: storage });
+
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-var User = require('./User');
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
@@ -25,8 +42,6 @@ router.post('/', function (req, res) {
             facebook_id: req.body.facebook_id,
             line_id: req.body.line_id,
             instagram_id: req.body.instagram_id,
-            profile_image: req.body.profile_image,
-            cover_image: req.body.cover_image,
             password: req.body.password
         }, 
         function (err, user) {
@@ -39,6 +54,12 @@ router.post('/', function (req, res) {
 router.get('/', function (req, res) {
     User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
+        users.forEach(function(part, index, theArray) {
+            var image = theArray[index].profile_image
+            if (image) {
+                theArray[index].profile_image = req.protocol + '://' + req.get('host') + "/" + image
+            }
+        });
         res.status(200).send(users);
     });
 });
@@ -69,4 +90,22 @@ router.put('/:id', function (req, res) {
     });
 });
 
+router.post('/upload_profile_image/:id', upload.single('profile_image'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    // let extArray = req.file.mimetype.split("/");
+    // let extension = extArray[extArray.length - 1];
+    // var profile_image = req.file.path + "." + extension;
+
+    let image_path = { profile_image: req.file.path };
+
+    User.findByIdAndUpdate(req.params.id, image_path , {new: true}, function (err, user) {
+        if (err) return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+    });
+})
+
+
 module.exports = router;
+
+
